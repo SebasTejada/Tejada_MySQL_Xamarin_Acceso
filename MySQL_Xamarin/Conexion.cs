24b7b9caeb3b55d7f07309d09b09ae96193554b1
SebasTejada <sebastian.tejada@udla.edu.ec>
2020-11-18 22:50:45 -0500

    Agregar archivos de proyecto.

---
+++ MySQL_Xamarin/Conexion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Android.Content;
+using Android.Widget;
+using MySql.Data.MySqlClient;
+
+namespace MySQL_Xamarin
+{
+	public class Conexion
+	{
+	/// <summary>
+	/// Prueba la conexión a la base de datos utilizando las credenciales correspondientes
+	/// </summary>
+	/// <param name="context"></param>
+	/// <param name="Usuario"></param>
+	/// <param name="Contrasenia"></param>
+	/// <returns></returns>
+		public bool TryConnection(Context context, string Usuario,string Contrasenia)
+		{
+			MySqlConnectionStringBuilder Builder = new MySqlConnectionStringBuilder();
+			Builder.Port = 3306;
+			Builder.Server = "sql9.freemysqlhosting.net"; //Al ser una BD Online debes usar la ip de tu servidor y no localhost
+			Builder.Database = "sql9377454";
+			Builder.UserID = Usuario; //Es el usuario de la base de datos
+			Builder.Password = Contrasenia; //La contraseña del usuario
+			try
+			{
+				MySqlConnection ms = new MySqlConnection(Builder.ToString());
+				ms.Open();
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Toast.MakeText(context,ex.ToString(), ToastLength.Long).Show(); //Muestra un Toast con el error (Puede ser muy largo)
+				return false;
+			}
+		}
+	}
+}
---
+++ MySQL_Xamarin/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace MySQL_Xamarin
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+		//Declaración de los componentes.
+		Button access;
+		EditText User, Password;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+			//Se asignan los controles del Layout a una varaible del mismo tipo
+			access = FindViewById<Button>(Resource.Id.btn_entrar);
+			User = FindViewById<EditText>(Resource.Id.et_user);
+			Password = FindViewById<EditText>(Resource.Id.et_password);
+
+			//La accion de presionar el boton
+			access.Click += delegate {
+			//Se crea una instancia de la clase Conexion
+				Conexion con = new Conexion();
+				//Se prueba la conexion pasando los datos de los EditText que son usuario y contraseña
+				if (con.TryConnection(this,User.Text,Password.Text)){
+					Toast.MakeText(this, "Conexion Exitosa!", ToastLength.Long).Show();
+				}
+				else{
+					Toast.MakeText(this, "Error!", ToastLength.Long).Show();
+				}
+			};
+		}
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
